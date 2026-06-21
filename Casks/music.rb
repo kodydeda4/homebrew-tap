734919625148dc83067f1109a895cb0e7b1660e5
kodydeda4 <kodydeda4@gmail.com>
2026-06-21 19:29:04 -0400

    push

--- Casks/music.rb
+++ Casks/music.rb
@@ -2,7 +2,8 @@ cask "music" do
   version "1.0.0"
   sha256 "282df7e0d9cf4785a63ae89e20b5a5cfe56057a22a4183c79052b35171d8d19d"
 
-  url "https://github.com/kodydeda4/music-player-releases/releases/download/v#{version}/Music.zip"
+  url "https://github.com/kodydeda4/music-player-releases/releases/download/v#{version}/Music.zip",
+      verified: "github.com/kodydeda4/music-player-releases/"
   name "Music"
   desc "One-line description of your app"
   homepage "https://kodydeda.vercel.app/music-app"
@@ -13,6 +14,7 @@ cask "music" do
   end
 
   auto_updates true
+  depends_on macos: ">= :sonoma"
 
   app "Music.app"
 end
